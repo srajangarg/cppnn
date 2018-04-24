32616af2f884d454ab9a3750bfc9ea199b838790
Srajan Garg <srajan.garg@gmail.com>
2018-04-25 02:18:48 +0530

    init backprop code

--- layer.h
+++ layer.h
@@ -10,12 +10,15 @@ public:
 
     float *in_matrix;
     float *out_matrix;
+    float *dc_do;
+    float *dc_di;
     int outputs;
     int inputs;
 
     virtual ~Layer()
     {
         delete[] out_matrix;
+        delete[] dc_do;
     }
 };
 
@@ -23,28 +26,45 @@ class Dense : public Layer
 {
 
 public:
-    float *wt_matrix;
+    float *wt_matrix; // input x output
+    float *bias;      // 1 x output
+    float *dc_dwt;    // input x output
+    float *dc_dbias;  // 1 x output
 
     Dense(int outs)
     {
         outputs = outs;
         out_matrix = new float[outs];
+        dc_do = new float[outputs];
     }
 
     void initialize()
     {
         wt_matrix = new float[inputs * outputs];
-        // initialize wt_matrix
+        bias = new float[outputs];
     }
 
     void forward()
     {
-        // in_matrix x wt_matrix = out_matrix
+        mat_mul(in_matrix, wt_matrix, out_matrix, 1, inputs, outputs);
+
+        for (int i = 0; i < outputs; i++)
+            out_matrix[i] += bias[i];
+    }
+
+    void backprop()
+    {
+        float* in_t = new float[inputs];
+
+        mat_mul();
+
+        delete in_t;
     }
 
     ~Dense()
     {
         delete[] wt_matrix;
+        delete[] dc_dwt;
     }
 };
 
