771007e121aebd85f595b56a3f72c919bdf2d495
Shubham Goel <sgoel160@gmail.com>
2018-05-06 13:18:58 +0530

    Saveing to JPG

--- test_conv.cu
+++ test_conv.cu
@@ -22,12 +22,12 @@ int main()
     kH = 9, kW = 9;
     generate_box_blur_kernel(kernel, inF, kH, kW, outF);
     func_conv2d(img, inF, H, W, kernel, outF, kH, kW, img1, kH / 2, kW / 2);
-    save_image("blur-box.bmp", img1, H, W, outF);
+    save_image("blur-box.jpg", img1, H, W, outF);
 
     sigma = 5, kH = 13, kW = 13;
     generate_gauss_blur_kernel(kernel, inF, kH, kW, outF, sigma);
     func_conv2d(img, inF, H, W, kernel, outF, kH, kW, img1, kH / 2, kW / 2);
-    save_image("blur-gauss.bmp", img1, H, W, outF);
+    save_image("blur-gauss.jpg", img1, H, W, outF);
 
     kH = 3, kW = 3;
     generate_Gx_kernel(kernel, inF, kH, kW, outF);
@@ -36,25 +36,25 @@ int main()
     func_conv2d(img, inF, H, W, kernel, outF, kH, kW, img2, kH / 2, kW / 2);
     img2.mag_components_(img1);
     normalize_scale_image(img2, inF, H, W, 255);
-    save_image("edge.bmp", img2, H, W, outF);
+    save_image("edge.jpg", img2, H, W, outF);
 
     kH = 3, kW = 3;
     generate_highpass_kernel(kernel, inF, kH, kW, outF);
     func_conv2d(img, inF, H, W, kernel, outF, kH, kW, img1, kH / 2, kW / 2);
     img1.clip_(0, 255);
-    save_image("highpass.bmp", img1, H, W, outF);
+    save_image("highpass.jpg", img1, H, W, outF);
 
     kH = 3, kW = 3;
     generate_sharpen_kernel(kernel, inF, kH, kW, outF);
     func_conv2d(img, inF, H, W, kernel, outF, kH, kW, img1, kH / 2, kW / 2);
     img1.clip_(0, 255);
-    save_image("sharpen.bmp", img1, H, W, outF);
+    save_image("sharpen.jpg", img1, H, W, outF);
 
     kH = 3, kW = 3;
     generate_emboss_kernel(kernel, inF, kH, kW, outF);
     func_conv2d(img, inF, H, W, kernel, outF, kH, kW, img1, kH / 2, kW / 2);
     img1.clip_(0, 255);
-    save_image("emboss.bmp", img1, H, W, outF);
+    save_image("emboss.jpg", img1, H, W, outF);
 
     // display_image(img1, H, W, outF);
 
